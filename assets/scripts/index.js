8842c098a63225d349e155dac02470cc252174b2
akadane87 <akadane87@gmail.com>
2016-12-16 16:07:01 -0500

    add product modal

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,14 +1,14 @@
 'use strict';
 
-const setAPIOrigin = require('../../lib/set-api-origin');
-const config = require('./config');
-const authEvents = require('../auth/events.js');
-const stripeEvents = require('./stripe/stripe-events.js');
-
-require('./assets/scripts/index.js');
+// const setAPIOrigin = require('../../lib/set-api-origin');
+// const config = require('./config');
+// const authEvents = require('../auth/events.js');
+// const stripeEvents = require('./stripe/stripe-events.js');
+//
+// require('./assets/scripts/index.js');
 
 $(() => {
-  setAPIOrigin(location, config);
-  stripeEvents.addHandlers();
-  authEvents.addHandlers();
+  // setAPIOrigin(location, config);
+  // stripeEvents.addHandlers();
+  // authEvents.addHandlers();
 });
